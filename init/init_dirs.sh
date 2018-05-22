3157422cbb8bb589b651dafd801234986a619e7e
campovski <campovski@gmail.com>
2018-05-22 14:15:21 +0200

    Add python and pip installation

--- init/init_dirs.sh
+++ init/init_dirs.sh
@@ -16,6 +16,9 @@ if [ "$1" == "1" ]; then
 
     # Fortran 77 installation
     sudo apt install -y fort77;
+    
+    # Install python and pip
+    sudo apt install -y python python3 python-dev python-pip
 
     # C# installation
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
