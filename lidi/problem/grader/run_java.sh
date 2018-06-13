9699e62e70e679970816e29ca7618c9ed0146c7e
Marcel Čampa <campovski@gmail.com>
2018-06-13 10:44:12 +0200

    Correct mistake in run_java.sh

--- lidi/problem/grader/run_java.sh
+++ lidi/problem/grader/run_java.sh
@@ -11,7 +11,7 @@ problem_dir=$CG_FILES_UPLOADED/$user/$problem;
 limit_t=$(cat $CG_FILES_PROBLEMS/$problem/limit_t);
 limit_m=$(cat $CG_FILES_PROBLEMS/$problem/limit_m);
 
-cmd_to_exec="/usr/local/bin/timeout -t ${limit_t} -m ${limit_m} bash /lidi_files/runners/run_py.sh ${1} ${2} ${main_class} 2> /lidi_files/out/timeout;";
+cmd_to_exec="/usr/local/bin/timeout -t ${limit_t} -m ${limit_m} bash /lidi_files/runners/run_java.sh ${1} ${2} ${main_class} 2> /lidi_files/out/timeout;";
 docker exec -t lidi_container_${user} /bin/bash -c "${cmd_to_exec}";
 
 rm $main_class;
